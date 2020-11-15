8eeed1de1a9cc21bcc576c2dec9d5b922ba3ba49
Myung-gwan Kim <cartopia95@naver.com>
2020-11-16 01:06:01 +0900

    add - helmet과 router 적용

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { Route } from 'react-router-dom';
+
+import './App.css';
+import HomePage from './pages/HomePage';
+import SummaryPage from './pages/SummaryPage';
+import WordCloudPage from './pages/WordCloudPage';
+
+function App () { 
+    return (
+        <>
+            <Helmet><title>GreatLaboratory</title></Helmet>
+            <Route component={HomePage} path='/' exact/>
+            <Route component={WordCloudPage} path={['/wordCloud/:category', '/wordCloud']}/>
+            <Route component={SummaryPage} path={['/summarization/:category', '/summarization']}/>
+        </>
+    );
+}
+
+export default App;
