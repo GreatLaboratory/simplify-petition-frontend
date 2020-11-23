7e02dee29f495e7ee923b5eae0675219c3ceef15
Myung-gwan Kim <cartopia95@naver.com>
2020-11-23 09:06:40 +0900

    add - 각 페이지에 router - match params 추가

--- src/pages/SummaryPage.tsx
+++ src/pages/SummaryPage.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { RouteComponentProps } from 'react-router-dom';
 import HeaderComponent from '../components/HeaderComponent';
 import CategoryContainer from '../containers/CategoryContainer';
 import SummaryContainer from '../containers/SummaryContainer';
 
-const SummaryPage = () => {
+type MatchParams = {
+    category: string;
+    petitionID: string;
+}
+
+const SummaryPage: React.FC<RouteComponentProps<MatchParams>> = ({ match }) => {
+    const category: string = match.params.category;
+    const petitionID: string = match.params.petitionID;
     return (
         <>
-            <Helmet><title>summary</title></Helmet>
+            <Helmet><title>summarization</title></Helmet> 
             <HeaderComponent/>
             <CategoryContainer/>
-            <SummaryContainer/>
+            <SummaryContainer category={category} petitionID={petitionID}/>
         </>
     );
 };
--- src/pages/WordCloudPage.tsx
+++ src/pages/WordCloudPage.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { RouteComponentProps } from 'react-router-dom';
 import HeaderComponent from '../components/HeaderComponent';
 import CategoryContainer from '../containers/CategoryContainer';
 import WordCloudContainer from '../containers/WordCloudContainer';
 
-const WordCloudPage = () => {
+type MatchParams = {
+    category: string;
+}
+
+const WordCloudPage: React.FC<RouteComponentProps<MatchParams>> = ({ match }) => {
+    const category: string = match.params.category;
     return (
         <>
             <Helmet><title>wordCloud</title></Helmet>
             <HeaderComponent/>
             <CategoryContainer/>
-            <WordCloudContainer/>
+            <WordCloudContainer category={category}/>
         </>
     );
 };
