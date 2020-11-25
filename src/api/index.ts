2b392bd22255c30c230b4dedcfd1d762960477d9
Myung-gwan Kim <cartopia95@naver.com>
2020-11-25 09:59:33 +0900

    add - 연동시킬 백엔드 api 불러오기

---
+++ src/api/index.ts
@@ -0,0 +1,57 @@
+import axios, { AxiosResponse } from 'axios';
+
+const flaskBaseUrl: string = 'http://localhost:5000';
+const echoBaseUrl: string = 'http://localhost:1323';
+export const getWordCloudImage = async (category: string) => {
+    try {
+        const imgUrl: string = `${flaskBaseUrl}/api/wordCloud/${category}`;
+        const response: AxiosResponse = await axios.get(imgUrl);
+        if (!response.data) throw new Error('data access error');
+        return imgUrl;
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const getTopFiveList = async (category: string) => {
+    try {
+        const response: AxiosResponse<Petition[]> = await axios.get(`${echoBaseUrl}/topFivePosts/${category}`);
+        return response.data;
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const getOrginalPetition = async (petitionID: string) => {
+    try {
+        const response: AxiosResponse<QnA> = await axios.get(`${echoBaseUrl}/summary/${petitionID}`);
+        return response.data;
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const getSummaryPetition = async (petitionID: string) => {
+    try {
+        const response: AxiosResponse<QnA> = await axios.get(`${flaskBaseUrl}/api/summary/${petitionID}`);
+        return response.data;
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export interface Petition {
+    id: string,
+    paging_id: number,
+    title: string,
+    agreement: string,
+    category: string,
+    created: string,
+    finished: string,
+    provider: string,
+}
+
+export interface QnA {
+    question: string,
+    answer: string,
+}
